9fc6d18f3b6453dd5de0a65d23e9de16e8a892d2
YKarsten <yannik.karsten@web.de>
2023-01-22 18:44:11 +0100

    Solve task 01

--- spec/01_string_spec.rb
+++ spec/01_string_spec.rb
@@ -39,18 +39,21 @@ end
 
 describe String do
   # Create a let variable that will pass the first test.
-
-  # remove the 'x' before running this test
-  xit 'is equal to tacos' do
-    expect(favorite_food).to eq('tacos')
+  context 'when I descibe a let variable' do
+    let(:favorite_food) { String.new('tacos') }
+    # remove the 'x' before running this test
+    it 'is equal to tacos' do
+      expect(favorite_food).to eq('tacos')
+    end
   end
 
   # remove the 'x' before running this test
   context 'when favorite food is updated' do
     # Change the favorite_food let variable.
-
-    xit 'updates the favorite food' do
+    let(:favorite_food) { String.new('pizza') }
+    it 'updates the favorite food' do
       # Write a test that will pass.
+      expect(favorite_food). to eql('pizza')
     end
   end
 end
